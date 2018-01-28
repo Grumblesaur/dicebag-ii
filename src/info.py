6b1fb4d65245f564cb509a495ba14ad6237b57fe
James Murphy <james.f.murphy.one@gmail.com>
2018-01-28 02:59:26 +0000

    add correct link

--- src/info.py
+++ src/info.py
@@ -1,7 +1,7 @@
 def scan(msg):
   out = ""
   if msg.startswith("!help"):
-    out = "https://github.com/Grumblesaur/dicebag-ii/README.md"
+    out = "https://github.com/Grumblesaur/dicebag-ii/blob/master/README.md"
   return out
 
 
