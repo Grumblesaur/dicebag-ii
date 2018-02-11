b8f3cd28449a16d636de33c06b6272d9276203e1
James Murphy <james.f.murphy.one@gmail.com>
2018-02-11 15:59:54 -0500

    add more insults and error messages

--- src/dice.py
+++ src/dice.py
@@ -1,6 +1,37 @@
 from engine import roll, ParseError
 from random import choice
 
+responses = [
+  "You really ate the bones, %s.",
+  "Yo, %s, would you kindly stop breaking things?",
+  "Don't waste my time, %s.",
+  "Fuck you, %s!",
+  "Sorry, %s, I'm not really feeling up to your bullshit today.",
+  "Hey %s! Read the documentation!",
+  "Keep your hands to yourself, %s!",
+  "Eyes on your own work there, %s!",
+  "Syntax error: go fuck yourself, %s.",
+  "java.lang.Exception: HAH! This is Python! You really screwed the pooch"
+    + " now, %s!",
+  "No, %s. Not gonna happen.",
+  "Well, %s, that was inconsiderate.",
+  "Could you behave for two seconds, %s?",
+  "How many times do we have to teach you this lesson, %s?",
+  "Must you break everything you touch, %s?",
+  "Do you live in a cave, %s?",
+  ]
+names = [
+  'jerk', 'fuckface', 'moron', 'dirtfarmer',
+  'dingus', 'useless', 'bonehead', 'milk-drinker',
+  'skeleton man', 'shitlord', 'bird brain',
+  'dick-twiddler', 'raggedy muffin child',
+  'shifty fister', 'ass-wrangler', 'jumbo toddler',
+  'turd-burglar', 'pit-sniffer', 'old fart',
+  'witless wonder', 'freakenstein', 'dingus khan',
+  'toad-tickler', 'dick chimp', 'mud stump',
+  'nerd-herder', 'turd-surfer', 'genius',
+  'Ed-boy'
+]
 def scan(msg, usr):
   if '!roll' not in msg:
     return []
@@ -17,32 +48,7 @@ def scan(msg, usr):
   return rolls
 
 def snark(username, error):
-  responses = [
-    "You really ate the bones, %s.",
-    "Yo, %s, would you kindly stop breaking things?",
-    "Don't waste my time, %s.",
-    "Fuck you, %s!",
-    "Sorry, %s, I'm not really feeling up to your bullshit today.",
-    "Hey %s! Read the documentation!",
-    "Keep your hands to yourself, %s!",
-    "Eyes on your own work there, %s!",
-    "Syntax error: go fuck yourself, %s.",
-    "java.lang.Exception: HAH! This is Python! You really screwed the pooch"
-      + " now, %s!",
-    "No, %s. Not gonna happen.",
-    "Well, %s, that was inconsiderate.",
-    "Could you behave for two seconds, %s?",
-    "How many times do we have to teach you this lesson, %s?"
-  ]
-  names = [
-    'jerk', 'fuckface', 'moron', 'dirtfarmer',
-    'dingus', 'useless', 'bonehead', 'milk-drinker',
-    'skeleton man', 'shitlord', 'bird brain',
-    'dick twiddler', 'raggedy muffin child',
-    'shifty fister', 'ass-wrangler', 'jumbo toddler',
-    'turd burglar', 'pit sniffer', 'old fart',
-    'witless wonder', 'freakenstein', 'dingus khan'
-  ]
+  
   
   return choice(responses) % choice(
     names + ([username] * (len(names) // 3))
