a421c01502a1fb6c00d8588599724e8b97d1c6d0
James Murphy <james.f.murphy.one@gmail.com>
2018-02-14 19:43:58 -0500

    test name sanitization live

--- src/names.py
+++ src/names.py
@@ -1,13 +1,18 @@
 defaults = {
   'race'              : '',
   'gender'            : '',
-  'first'             : True,
-  'last'              : True,
+  'types'             : ['first'],
   'first starts with' : '',
   'last starts with'  : '',
   'affix'             : False,
   'subrace'           : ''
 }
+
+string_fields = (
+  'race', 'gender', 'first starts with', 'last starts with', 'subrace'
+)
+
+categories = ('first', 'last')
 
 sans_last = (
   'altmer', 'argonian', 'bosmer', 
@@ -27,11 +32,26 @@ options = {
   )
 }
 
+def downcase(namespec):
+  for key in namespec:
+    namespec[key.casefold()] = namespec[key]
+    del namespec[key]
+  for key in string_fields:
+    namespec[key] = namespec[key].casefold()
+  namespec['types'] = [t.casefold() for t in namespec['types']]
+  return downcase
+
 def sanitize(namespec):
   # ensure that all fields are supplied
   for key in defaults:
+    if key == 'types':
+      continue
     if key not in namespec:
       namespec[key] = defaults[key]
+  
+  for nametype in namespec['types']:
+    if nametype not in categories:
+      namespec['types'].pop(nametype)
   
   for key in options:
     if namespec[key] not in options[key]:
@@ -53,7 +73,7 @@ def sanitize(namespec):
   if not namespec['first starts with'].isalpha():
     namespec['first starts with'] = ''
   if not namespec['last stats with'].isalpha():
-    namespec['last starts with' = ''
+    namespec['last starts with'] = ''
   
   return namespec
   
