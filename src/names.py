4eb15f4d2ff048d6c1980ee110a521f4f0f239e6
James Murphy <james.f.murphy.one@gmail.com>
2018-02-15 04:58:26 +0000

    sanitize pipeline is error-free

--- src/names.py
+++ src/names.py
@@ -32,22 +32,28 @@ options = {
   )
 }
 
+def sanitize(namespec):
+  x = normalize(namespec)
+  y = downcase(x)
+  return correct(y)
+
 def normalize(namespec):
   for key in defaults:
     if key not in namespec:
       namespec[key] = defaults[key]
-  return namspec
+  return namespec
 
 def downcase(namespec):
+  temp = namespec
   for key in namespec:
-    namespec[key.casefold()] = namespec[key]
-    del namespec[key]
+    temp[key.casefold()] = namespec[key]
+  namespec = temp
   for key in string_fields:
     namespec[key] = namespec[key].casefold()
   namespec['types'] = [t.casefold() for t in namespec['types']]
-  return downcase
+  return namespec
 
-def sanitize(namespec):
+def correct(namespec):
   for nametype in namespec['types']:
     if nametype not in categories:
       namespec['types'].pop(nametype)
@@ -55,7 +61,6 @@ def sanitize(namespec):
   for key in options:
     if namespec[key] not in options[key]:
       namespec[key] = ''
-  
   # correct for a possible common spelling mistake
   if namespec['subrace'] == 'cyrodiilic':
     namespec['subrace'] = 'cyrodilic'
@@ -71,14 +76,9 @@ def sanitize(namespec):
   
   if not namespec['first starts with'].isalpha():
     namespec['first starts with'] = ''
-  if not namespec['last stats with'].isalpha():
+  if not namespec['last starts with'].isalpha():
     namespec['last starts with'] = ''
   
   return namespec
   
   
-
-if __name__ == '__main__':
-  print(sanitize(downcase({ })))
-
-
