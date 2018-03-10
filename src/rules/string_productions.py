1cc1b2b011116b7052b9cd76fd7a93c40a07e8c0
James Murphy <james.f.murphy.one@gmail.com>
2018-03-09 21:48:53 -0500

    I'm stupid and don't know how backslashes work

--- src/rules/string_productions.py
+++ src/rules/string_productions.py
@@ -1,10 +1,10 @@
 def p_red(tokens):
   '''expr : RED expr'''
-  tokens[0] = "```diff\\n- %s```" % str(tokens[2])
+  tokens[0] = "```diff\n- %s```" % str(tokens[2])
 
 def p_green(tokens):
   '''expr : GREEN expr'''
-  tokens[0] = "```diff\\n+ %s```" % str(tokens[2])
+  tokens[0] = "```diff\n+ %s```" % str(tokens[2])
 
 def p_gray(tokens):
   '''expr : GRAY expr'''
