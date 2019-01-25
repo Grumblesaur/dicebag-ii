1d6081a4b3fa3082f8a3013b2f51b61f59e7f662
James Murphy <james.f.murphy.one@gmail.com>
2019-01-24 22:12:51 -0500

    changed up.sh

--- up.sh
+++ up.sh
@@ -1,5 +1,12 @@
 cd src
-until python3 main.py; do
-	echo "Process 'dicebag.py' crashed with exit code $?. Respawning..." >&2
-	sleep 1
+while true; do
+  python3 main.py
+  # Give three seconds for operator to stop loop code.
+  echo "Dicebag stopped with exit code $?. Respawning in 3..."
+  sleep 1
+  echo "2..."
+  sleep 1
+  echo "1..."
+  sleep 1
 done
+
